d7d0edf7aeab9ff8b6e6f1352a2ff2956d299e5a
Erivan Rodrigues <erivanrgj@gmail.com>
2016-04-17 10:59:45 -0300

    Add test todo post

--- src/test/java/cobr/edu/cesmac/AppTest.java
+++ src/test/java/cobr/edu/cesmac/AppTest.java
@@ -7,11 +7,12 @@ import org.junit.Test;
  */
 public class AppTest extends BaseTest {
 
+
   @Test
-  public void index() throws Exception {
-    server.get("/")
-        .expect(200)
-        .header("Content-Type", "text/html;charset=UTF-8");
+  public void newTodo() throws Exception {
+	  server.post("/todo").body("{\"name\":\"eriva\"}", "application/json")
+	  .expect("{\"name\":\"eriva\",\"id\":1}");
+	  
   }
 
 }
